6c485828a2f411a698f25254593037cf79375da5
Jeffrey Duivenvoorden <jheremenis@gmail.com>
2025-04-23 11:42:20 +0200

    move page to page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+
+<div id="gameArea">
+    <canvas id="world" width="800" height="600"></canvas>
+    <div id="instructions">
+      <b>A/D</b> to move, <b>W</b> to jump, collect treasure.<br />
+      <b>Wall jump</b>, by coming close to a cornerstone and walking against
+      it and jump<br />
+      <b>Death</b> your deaths are stored inside cookies, don't die much or
+      you don't get a reward (10times the limit) <br /><br />
+      To add your score to the board email "jheremenis@gmail.com" with your
+      score, desired name and a printscreen image of your victory sign
+    </div>
+    <div id="instructions">
+      <h2>ScoreBoard</h2>
+      <b>#1</b>Jeffrey Duivenvoorden: 14 keer gefaald<br />
+      <b>#2</b>Jimmy Konijnenburg: 48 keer gefaald<br />
+      <b>#3</b>Robby uit de Hoogeboom gevallen: 57 keer gefaald volgens figo's<br />
+
+      <h5>All time zielig:</h5>
+      <b>#4</b>Figo Hulsebosch: 182 keer gefaald<br />
+    </div>
+  </div>
+
+  <audio
+    style="width: 100%; position: fixed; bottom: 0px; right: 0px"
+    controls="controls"
+    playsinline=""
+    autoplay="autoplay"
+    loop=""
+  >
+    <source src="heyya.mp3" type="audio/mpeg" />
+  </audio>
